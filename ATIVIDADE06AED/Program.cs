6339df55445d7155a69575df6c7ef3d6a3f4964c
vgpur <vgpur@LAPTOP-HR52MGN3>
2023-03-28 21:33:38 -0300

    Add project files.

---
+++ ATIVIDADE06AED/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = new int[7];
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.WriteLine("Digite um número: ");
+            nums[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] distinctNums = nums.Distinct().ToArray();
+        distinctNums = distinctNums.Where(num => num % 2 != 0).ToArray();
+        distinctNums.Reverse();
+        foreach(int num in distinctNums)
+            {
+            Console.WriteLine(num);
+        }
+    }
+}
